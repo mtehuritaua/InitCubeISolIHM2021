d0889edf54b4099eb3748a0c8aa1cac5f404abd3
mtehuritaua <mtehuritaua@btslivh.eu>
2021-03-23 17:39:23 +0100

    Création de gestionnaire d'instruments

--- InitCubeSolIHM-master/js/majEtat.js
+++ InitCubeSolIHM-master/js/majEtat.js
@@ -2,71 +2,77 @@
 //var matrice = new CMatrice(camera);
 
 $(document).ready(function() {
-/*-------------------------------------Graphiques de la page Etat-------------------------------------------*/
-    let graphBattCharge = new Graphique("graphique","Etat","Batterie", "Charge","%");
-    let graphBattTension = new Graphique("graphique","Etat","Batterie", "Tension","V");
-    let graphBattCourant = new Graphique("graphique","Etat","Batterie", "Courant","A");
-    let graphRAMuse = new Graphique("graphique","Etat","RAM", "Occupation","%");
-    let graphRAMdispo = new Graphique("graphique","Etat","RAM", "Disponile","Mo");
-    let graphStockLibreP = new Graphique("graphique","Etat","Stockage", "Libre","%");
-    let graphStockLibreM = new Graphique("graphique","Etat","Stockage", "Disponible","Mo");
+
+    /*-------------------------------------Gestionnaire d'Instrument---------------------------------------------*/
+        let gestionnaireInstruments = new GestionnaireInstruments("../initcube.xml");
     
-/*--------------------------------------Graphiques de la page Magnétomètre--------------------------------------------*/
-    let graphMagnetoBX = new Graphique("graphMagneto","Magnetometre","ValeurBX","Valeur","μT");
-    let graphMagnetoBY = new Graphique("graphMagneto","Magnetometre","ValeurBY","Valeur","μT");
-    let graphMagnetoBZ = new Graphique("graphMagneto","Magnetometre","ValeurBZ","Valeur","μT");
     
-    var source = new EventSource("/cgi-bin/cubeEventServer.cgi");
-    source.addEventListener("etat", function(event) {
-    var obj = JSON.parse(event.data);
-    document.getElementById("ChargeBatterie").innerHTML = obj.batterie.niveauDeCharge + " %";
-    graphBattCharge.ajouterMesure(obj.date,obj.batterie.niveauDeCharge);
-    document.getElementById("TensionSortie").innerHTML = obj.batterie.tension + " V";
-    graphBattTension.ajouterMesure(obj.date,obj.batterie.tension);
-    document.getElementById("CourantSortie").innerHTML = obj.batterie.courant + " mA";
-    graphBattCourant.ajouterMesure(obj.date,obj.batterie.courant);
-    document.getElementById("RamUse%").innerHTML = obj.memoire.occupMemoire + " %";
-    graphRAMuse.ajouterMesure(obj.date,obj.memoire.occupMemoire);
-    document.getElementById("RamDispo").innerHTML = obj.memoire.memoireDispoMo + " Mo";
-    graphRAMdispo.ajouterMesure(obj.date,obj.memoire.memoireDispoMo);
-    document.getElementById("StockLibre%").innerHTML = obj.stockage.stockLibreEnP + " %";
-    graphStockLibreP.ajouterMesure(obj.date,obj.stockage.stockLibreEnP);
-    document.getElementById("StockLibreMo").innerHTML = obj.stockage.stockLibreMo + " Mo";
-    graphStockLibreM.ajouterMesure(obj.date,obj.stockage.stockLibreMo);
-    document.getElementById("InfoCamera1").innerHTML = obj.camera.InfoCamera1;
-    document.getElementById("InfoCamera2").innerHTML = obj.camera.InfoCamera2;
-
-    if (obj.cameraIR == 0) {
-        document.getElementById("CameraIR").innerHTML = "OFF";
-    } else {
-        document.getElementById("CameraIR").innerHTML = "ON";
-    }
-});
-
-source.addEventListener("instrument", function(evt){
-    var instru = JSON.parse(evt.data);
-    var camera = new CCamera();
-    var matrice = new CMatrice(camera);
-    switch (instru.instrument.type) {
-        case "matrice":
-            camera.setPixel(instru.instrument.mesure);
-            matrice.majMatrice();
-            break;
-        case "magneto":
-            graphMagnetoBX.ajouterMesure(instru.instrument.date,instru.instrument.mesure.ValeurMagnetoBX);
-            graphMagnetoBY.ajouterMesure(instru.instrument.date,instru.instrument.mesure.ValeurMagnetoBY);
-            graphMagnetoBZ.ajouterMesure(instru.instrument.date,instru.instrument.mesure.ValeurMagnetoBZ);
-
-            document.getElementById("ValeurMagnetoBX").innerHTML = instru.instrument.mesure.ValeurMagnetoBX + " μT";
-            document.getElementById("ValeurMagnetoBY").innerHTML = instru.instrument.mesure.ValeurMagnetoBY + " μT";
-            document.getElementById("ValeurMagnetoBZ").innerHTML = instru.instrument.mesure.ValeurMagnetoBZ + " μT";
-            
-            
-            break;
-        default:
-            console.log("Erreur d'identification de l'instrument");
-            console.log(instru.instrument.type);
-    }
-});
-
-});
+    /*-------------------------------------Graphiques de la page Etat-------------------------------------------*/
+        let graphBattCharge = new Graphique("graphique","Etat","Batterie", "Charge","%");
+        let graphBattTension = new Graphique("graphique","Etat","Batterie", "Tension","V");
+        let graphBattCourant = new Graphique("graphique","Etat","Batterie", "Courant","A");
+        let graphRAMuse = new Graphique("graphique","Etat","RAM", "Occupation","%");
+        let graphRAMdispo = new Graphique("graphique","Etat","RAM", "Disponile","Mo");
+        let graphStockLibreP = new Graphique("graphique","Etat","Stockage", "Libre","%");
+        let graphStockLibreM = new Graphique("graphique","Etat","Stockage", "Disponible","Mo");
+        
+    /*--------------------------------------Graphiques de la page Magnétomètre--------------------------------------------*/
+        let graphMagnetoBX = new Graphique("graphMagneto","Magnetometre","ValeurBX","Valeur","μT");
+        let graphMagnetoBY = new Graphique("graphMagneto","Magnetometre","ValeurBY","Valeur","μT");
+        let graphMagnetoBZ = new Graphique("graphMagneto","Magnetometre","ValeurBZ","Valeur","μT");
+        
+        var source = new EventSource("../cgi-bin/cubeEventServer.cgi");
+        source.addEventListener("etat", function(event) {
+        var obj = JSON.parse(event.data);
+        document.getElementById("ChargeBatterie").innerHTML = obj.batterie.niveauDeCharge + " %";
+        graphBattCharge.ajouterMesure(obj.date,obj.batterie.niveauDeCharge);
+        document.getElementById("TensionSortie").innerHTML = obj.batterie.tension + " V";
+        graphBattTension.ajouterMesure(obj.date,obj.batterie.tension);
+        document.getElementById("CourantSortie").innerHTML = obj.batterie.courant + " mA";
+        graphBattCourant.ajouterMesure(obj.date,obj.batterie.courant);
+        document.getElementById("RamUse%").innerHTML = obj.memoire.occupMemoire + " %";
+        graphRAMuse.ajouterMesure(obj.date,obj.memoire.occupMemoire);
+        document.getElementById("RamDispo").innerHTML = obj.memoire.memoireDispoMo + " Mo";
+        graphRAMdispo.ajouterMesure(obj.date,obj.memoire.memoireDispoMo);
+        document.getElementById("StockLibre%").innerHTML = obj.stockage.stockLibreEnP + " %";
+        graphStockLibreP.ajouterMesure(obj.date,obj.stockage.stockLibreEnP);
+        document.getElementById("StockLibreMo").innerHTML = obj.stockage.stockLibreMo + " Mo";
+        graphStockLibreM.ajouterMesure(obj.date,obj.stockage.stockLibreMo);
+        document.getElementById("InfoCamera1").innerHTML = obj.camera.InfoCamera1;
+        document.getElementById("InfoCamera2").innerHTML = obj.camera.InfoCamera2;
+    
+        if (obj.cameraIR == 0) {
+            document.getElementById("CameraIR").innerHTML = "OFF";
+        } else {
+            document.getElementById("CameraIR").innerHTML = "ON";
+        }
+    });
+    
+    source.addEventListener("instrument", function(evt){
+        var instru = JSON.parse(evt.data);
+        var camera = new CCamera();
+        var matrice = new CMatrice(camera);
+        switch (instru.instrument.type) {
+            case "matrice":
+                camera.setPixel(instru.instrument.mesure);
+                matrice.majMatrice();
+                break;
+            case "magneto":
+                graphMagnetoBX.ajouterMesure(instru.instrument.date,instru.instrument.mesure.ValeurMagnetoBX);
+                graphMagnetoBY.ajouterMesure(instru.instrument.date,instru.instrument.mesure.ValeurMagnetoBY);
+                graphMagnetoBZ.ajouterMesure(instru.instrument.date,instru.instrument.mesure.ValeurMagnetoBZ);
+    
+                document.getElementById("ValeurMagnetoBX").innerHTML = instru.instrument.mesure.ValeurMagnetoBX + " μT";
+                document.getElementById("ValeurMagnetoBY").innerHTML = instru.instrument.mesure.ValeurMagnetoBY + " μT";
+                document.getElementById("ValeurMagnetoBZ").innerHTML = instru.instrument.mesure.ValeurMagnetoBZ + " μT";
+                
+                
+                break;
+            default:
+                console.log("Erreur d'identification de l'instrument");
+                console.log(instru.instrument.type);
+        }
+    });
+    
+    });
+    

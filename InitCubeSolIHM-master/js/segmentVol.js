f6f048a6083322b88ab667b6ef07481c382b041c
mtehuritaua <mtehuritaua@btslivh.eu>
2021-04-01 14:39:59 +0200

    requete ajax fichierConf

--- InitCubeSolIHM-master/js/segmentVol.js
+++ InitCubeSolIHM-master/js/segmentVol.js
@@ -3,54 +3,29 @@ class SegmentVol {
         console.log("instance d'un gestionnaire d'instruments url du fichier : " + urlFichierConf);
         this.urlFichierConf = urlFichierConf;
         this.listeInstruments = new Array();
-        this.fichierConf = this.recupererFichierConf(this.chargerInstruments);
+        this.fichierConf;
+        this.recupererFichierConf();
+        this.chargerInstruments();
         console.log("<canvas id=\"instru" + this.type + this.source + "\"></canvas>");
         $("#instru").append("<div class=\"instruments\"><canvas id=\"instru" + this.type + "\"></canvas></div>");
     }
 
-    recupererFichierConf(fonctionRappel) {
+    recupererFichierConf() {
          $.ajax({
             type: "GET",
             url: this.urlFichierConf,
             dataType: "xml",
-            success: fonctionRappel
-                /*$(data).find('instrument').each(
-                    function () {
-            
-                        var id = $(this).find('id').text();
-                        console.log("l'identifiant de l'instrument : " + id);
-                        var nom = $(this).find('nom').text();
-                        console.log("nom de l'instrument : " + nom);
-                        var role = $(this).find('role').text();
-                        console.log("role de l'instrument : " + role);
-            
-                        this.listeInstruments.push(Instrument(id,nom,role));
-                        
-                        this.fichierConf.find('typeMesure').each(
-                        function(){
-                            var nom = $(this).find(nom).text();
-                            console.log("nom du type de mesure : " + nom);
-                            var description = $(this).find(description).text();
-                            console.log("description du type de mesure : " + description);
-                            var unite = $(this).find(unite).text();
-                            console.log("unite du type de mesure : " + unite);
-                            var valMin = $(this).find(valMin).text();
-                            console.log("valMin du type de mesure : " + valMin);
-                            var valMax = $(this).find(valMax).text();
-                            console.log("valMax du type de mesure : " + valMax);
-            
-                            this.listeInstruments[listeInstruments.length()-1].addTypeMesure(nom,description,unite,valMin,valMax);
-                        });
-                        console.log("caracteristique de l'instrument trouve dans le fichier de conf : ");
-                        console.log(this.listeInstruments[listeInstruments.length()-1].genererJSON());
-                    });*/
+            async:false,
+            success: function (fichierXML){
+                this.fichierConf = $(fichierXML);
+            }
         });
         console.log("fichier XML : " + fichierXML);
         //return fichierXML;
     }
 
-    chargerInstruments(xml){
-        xml.find('instrument').each(
+    chargerInstruments(){
+        fichierConf.find('instrument').each(
         function () {
 
             var id = $(this).find('id').text();

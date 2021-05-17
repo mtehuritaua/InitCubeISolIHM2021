3f48ac7a52e0f9382468333b946dff428323bea0
nberberian <nberberian@btslivh.eu>
2021-05-17 16:50:08 +0200

    modification gestionnaire commande

--- InitCubeSolIHM-master/js/gestionnaireCommandes.js
+++ InitCubeSolIHM-master/js/gestionnaireCommandes.js
@@ -16,7 +16,7 @@ class GestionnaireCommandes {
             url: 'cgi-bin/cgi_1',
             data: this.listeCommandes[this.listeCommandes.lenght - 1].genererJSON(),
             dataType: 'json',
-            success: function(codeRecu) {
+            success: function (codeRecu) {
                 popup(codeRecu)
 
             }
@@ -24,37 +24,41 @@ class GestionnaireCommandes {
     }
 
     getHistorique() {
-        // $(document).ready(function() {
-        let ths = this;
-        $.ajax({
-            type: 'GET',
-            url: 'cgi-bin/main',
-            dataType: 'html',
-            success: function(codeRecu) {
-                var tramesJson = new Array();
-                tramesJson = codeRecu.split('\n');
+        $(document).ready(function () {
+            let ths = this;
+            $.ajax({
+                type: 'GET',
+                url: 'cgi-bin/main',
+                dataType: 'html',
+                success: function (codeRecu) {
+                    var tramesJson = new Array();
+                    tramesJson = codeRecu.split('\n');
 
-                tramesJson.forEach(function(element) {
-                    var parse = $.parseJSON(element);
-                    //$("#listeHC").append('<li>' + element + '</li>');
-                    ths.historique.push(new Commande(parse.CMD.ID, parse.CMD.TYPE, "0" , parse.CMD.TYPEMEASURE));
-                });
-            }
+                    tramesJson.forEach(function (element) {
+                        var parse = $.parseJSON(element);
+                        //$("#listeHC").append('<li>'+element+'</li>');
+                        //ths.historique.push(element);
+                        const p //= new Commande(parse.CMD.ID, parse.CMD.TYPE, parse.CMD.TYPEMEASURE, parse.CMD.TYPEMEASURE)
+                        ths.addToHistorique(new Commande(parse.CMD.ID, parse.CMD.TYPE, parse.CMD.TYPEMEASURE, parse.CMD.TYPEMEASURE))
+                        //$("#listeHC").append('<li>'+p.genererJSON+'</li>');
+                    });
+                }
+            });
+
         });
-        //});
     }
 
-    addToHistorique(commande){
-
+    addToHistorique(commande) {
+        let ths = this;
+        ths.historique.push(commande);
     }
 
     afficherHistorique() { //Affiche l'objet de type commande dans le tableau historique
         let ths = this;
-        ths.historique.forEach(function(element) {
-            $("#listeHC").append('<li>' + element.genererJSON + '</li>');
-            //$("#listeHC").append(element.genererJSON);
-            //$("#listeHC").append('</li>');
+        ths.historique.forEach(function (element) {
+            
         })
+        $("#listeHC").append('<li>test</li>');
     }
 
 }

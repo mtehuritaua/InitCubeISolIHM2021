ce1521e2b133a7d14baa1ba524f5145db8fe75af
sCathalo <scathalo@btslivh.eu>
2021-06-02 14:57:56 +0200

    bloquer champs formulaire

--- InitCubeSolIHM-master/js/instrument/vueNouvelleInstrument.js
+++ InitCubeSolIHM-master/js/instrument/vueNouvelleInstrument.js
@@ -66,4 +66,82 @@ class VueNouvelleInstrument {
       $("#addRecap1").remove().prop("id", "addRecap" + gestionnaireCourant.nbTypesMesure--);
     });
   }
-}
+}/*
+$(document).ready(function () {
+  $("#formulaire").validate({
+    rules: {
+      nom: {
+        required: true,
+      },
+      identifiant: {
+        minlength: 6,
+        maxlength: 6,
+        required: true,
+      },
+      ref: {
+        required: true,
+      },
+      adresse: {
+        minlength: 4,
+        maxlength: 4,
+        required: true,
+      },
+      role: {
+        required: true,
+      },
+      nomMesure: {
+        required: true,
+      },
+      codeMesure: {
+        minlength: 2,
+        maxlength: 3,
+        required: true,
+      },
+      description: {
+        required: true,
+      },
+      unite: {
+        required: true,
+      },
+    },
+    messages: {
+      nom: {
+        required: "Entrez le nom de l'instrument.",
+      },
+      identifiant: {
+        minlength: "Au moins 6 caractères",
+        maxlength: "Au max 6 caractères",
+        required: "Entrez un identifiant composé de 3 chiffres et 3 lettres.",
+      },
+      ref: {
+        required: "Entrez une référence.",
+      },
+      adresse: {
+        minlength: "Au moins 4 caractères",
+        maxlength: "Au max 4 caractères",
+        required: "Entrez une adresse commençant par 0x..",
+      },
+      role: {
+        required: "Entrez le role de l'instrument.",
+      },
+      nomMesure: {
+        required: "Entrez le nom de la mesure.",
+      },
+      codeMesure: {
+        minlength: "Au moins 2 caractères",
+        maxlength: "Au max 3 caractères",
+        equalTo: "Entrez un code mesure",
+      },
+      description: {
+        required: "Entrez la description de cette mesure.",
+      },
+      unite: {
+        required: "Entrez l'unité utilisé pour cette mesure.",
+      },
+    },
+    errorPlacement: function (error, element) {
+      error.appendTo(element.parent());
+    },
+  });
+});*/
+
--- InitCubeSolIHM-master/js/segmentVol.js
+++ InitCubeSolIHM-master/js/segmentVol.js
@@ -1,11 +1,11 @@
 class SegmentVol {
     constructor(urlFichierConf) {
-      //  console.log("instance d'un gestionnaire d'instruments url du fichier : " + urlFichierConf);
+        //console.log("instance d'un gestionnaire d'instruments url du fichier : " + urlFichierConf);
         this.urlFichierConf = urlFichierConf;
         this.listeInstruments = new Array();
         this.fichierConf = this.recupererFichierConf();
         this.chargerInstruments();
-       // console.log("<canvas id=\"instru" + this.type + this.source + "\"></canvas>");
+        //console.log("<canvas id=\"instru" + this.type + this.source + "\"></canvas>");
         $("#instru").append("<div class=\"instruments\"><canvas id=\"instru" + this.type + "\"></canvas></div>");
     }
 
@@ -29,42 +29,44 @@ class SegmentVol {
         this.fichierConf.find('instrument').each(
         function () {
 
+            var nom = $(this).find('description').find('nom').text();
+            //console.log("nom de l'instrument : " + nom);
             var id = $(this).find('description').find('id').text();
             //console.log("l'identifiant de l'instrument : " + id);
             var ref = $(this).find('description').find('ref').text();
-           // console.log("référence de l'instrument : " + ref);
-            var nom = $(this).find('description').find('nom').text();
-           // console.log("nom de l'instrument : " + nom);
+            //console.log("référence de l'instrument : " + ref);
+            var adresse = $(this).find('description').find('adresse').text();
+            //console.log("référence de l'instrument : " + ref);
             var role = $(this).find('description').find('role').text();
-           // console.log("role de l'instrument : " + role);
+            //console.log("role de l'instrument : " + role);
 
-            segmentVolCourant.listeInstruments.push(new Instrument(nom, ref, role, id));
+            segmentVolCourant.listeInstruments.push(new Instrument(nom, id, ref, adresse, role));
             
             $(this).find('typeMesure').each(
             function(){
 
                 var code = $(this).find('code').text();
-             //   console.log("code du type de mesure : " + code);
+                //console.log("code du type de mesure : " + code);
                 var nom = $(this).find('nom').text();
-              //  console.log("nom du type de mesure : " + nom);
+                //console.log("nom du type de mesure : " + nom);
                 var description = $(this).find('description').text();
-              //  console.log("description du type de mesure : " + description);
+                //console.log("description du type de mesure : " + description);
                 var unite = $(this).find('unite').text();
-               // console.log("unite du type de mesure : " + unite);
+                //console.log("unite du type de mesure : " + unite);
                 var valMin = $(this).find('valMin').text();
-               // console.log("valMin du type de mesure : " + valMin);
+                //console.log("valMin du type de mesure : " + valMin);
                 var valMax = $(this).find('valMax').text();
                 //console.log("valMax du type de mesure : " + valMax);
 
                 segmentVolCourant.listeInstruments[segmentVolCourant.listeInstruments.length-1].addTypeMesure(code,nom,description,unite,valMin,valMax);
             });
            // console.log("caracteristique de l'instrument trouve dans le fichier de conf : ");
-           // console.log(segmentVolCourant.listeInstruments[segmentVolCourant.listeInstruments.length-1].genererJSON());
+            //console.log(segmentVolCourant.listeInstruments[segmentVolCourant.listeInstruments.length-1].genererJSON());
         });
     }
 
     genererMenuInstruments() {
-        //console.log("Nouvel Instrument du fichier : initcube.xml");
+       // console.log("Nouvel Instrument du fichier : initcube.xml");
         for(var i = 0;i<this.listeInstruments.length;i++){
             $("#Instrument").append('<div class="Instrument_1"><a href="#page'+this.listeInstruments[i].ref+'"><img src="images/'+this.listeInstruments[i].ref+'.png"/><br/><span>'+this.listeInstruments[i].nom+'</span></a></div>');
         }
